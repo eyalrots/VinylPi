d54d03fb73e506d598107b55a6f49fa3e8b7c401
eyalrots <eyalrots@gmail.com>
2025-04-26 20:55:31 +0100

    song recognition works - need gui

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ import json         # handles json files
 import io           # provides a consistant interface for wroking with input/output
 import os           # the way to interact with the operating system for vaios reasons
 import threading    # careate and manage threads withing the process
+import requests
+from shazamio import Shazam
 
 # Now that we have the right libraries, we can go ahead and write the code that will
 # aventually record the audio toa WAV file.
@@ -16,8 +18,6 @@ os.environ['PA_ALSA_PLUGHW'] = '1'   # tells ALSA to use audio card 0 (my mic)
 
 # Load configuration from a JSON file - for now it's only for the recording part
 def load_config():
-    #dir_path = os.path.dirname(os.path.abspath(__file__))
-    #config_path = os.path.join(dir_path, 'config.json')
     with open('./config.json', 'r') as config_file:
         return json.load(config_file)
     
@@ -72,7 +72,6 @@ def record_audio():
         if dev_index is None:
             return None
 
-    #script_dir = os.path.dirname(os.abspath(__file__))
     wav_output_filename = os.path.join('./', 'shazam.wav')
 
     audio = pyaudio.PyAudio()
@@ -106,9 +105,43 @@ def record_audio():
 
     return wav_output_filename
 
-if __name__ == "__main__":
-    recorded_file = record_audio()
-    if recorded_file:
-        print(f"Successfully recorded to {recorded_file}")
+async def recognize_song(wav_file):
+    shazam = Shazam()
+    max_retries = config['network']['retry_count']
+    for attempt in range(max_retries):
+        try:
+            return await shazam.recognize(wav_file)
+        except Exception as e:
+            print(f"Failed to recognize the song. Retrying... (Attempt {attempt+1}/{max_retries})")
+            await asyncio.sleep(config['network']['retry_delay'])
+    print("Max retry attempts reached. Could not recognize the song.")
+    return None
+
+async def update_song_information():
+    print("attempting to record song...")
+    wav_file = record_audio()
+    if wav_file is not None:
+        result = await recognize_song(wav_file)
+        if result:
+            print_info(result)
     else:
-        print("Recording failed.")
+        await update_song_information()
+
+def run_recognition_loop():
+    while True:
+        print("attempting getting information...")
+        asyncio.run(update_song_information())
+
+#def start_recognition_thread():
+#    print("starting thread...")
+#    thread = threading.Thread(target=run_recognition_loop)
+#    thread.daemon = True
+#    thread.start()
+
+def print_info(result):
+    if 'track' in result:
+        track_title = result['track']['title']
+        artist_name = result['track']['subtitle']
+        print(f"title: {track_title} :: artist: {artist_name}")
+
+run_recognition_loop()
